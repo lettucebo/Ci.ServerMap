325715b1c3a96b2b50b0f31cc879e9bd68f5da3e
Money <abc12207@gmail.com>
2019-10-17 22:19:45 +0800

    add project

---
+++ src/Ci.Web.MapPath/Enums/RootPathType.cs
@@ -0,0 +1,15 @@
+﻿namespace Ci.ServerMap.Enums
+{
+    public enum RootPathType
+    {
+        /// <summary>
+        /// Normal application root
+        /// </summary>
+        ContentRoot = 1,
+
+        /// <summary>
+        /// For asp.net core wwwroot
+        /// </summary>
+        WebRoot = 2
+    }
+}
---
+++ src/Ci.Web.MapPath/ServerPath.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Ci.ServerMap.Enums;
+
+namespace Ci.ServerMap
+{
+    public static class ServerMap
+    {
+        private static readonly string RootPath = Directory.GetCurrentDirectory();
+
+        public static string VirtualAbsolutePath(string virtualAbsolutePath,
+            RootPathType rootPathType = RootPathType.ContentRoot)
+        {
+            if (!virtualAbsolutePath.StartsWith("~/"))
+                throw new InvalidOperationException($"{nameof(virtualAbsolutePath)} not start with ~/");
+
+            string wwwrootPath = rootPathType == RootPathType.WebRoot ? "wwwroot" : string.Empty;
+
+            var path = Path.Combine(RootPath, wwwrootPath, virtualAbsolutePath.Replace("~/", ""));
+            return path;
+        }
+    }
+}
